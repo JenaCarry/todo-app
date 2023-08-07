642dfd93b8d82865251ab42dd4161839b34e4c3c
Jean <120676537+JenaCarry@users.noreply.github.com>
2023-08-07 20:13:18 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,10 @@
-"use client";
 import { Header } from "@/components/Header";
 import { Main } from "@/components/Main";
-import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [isLightTheme, setIsLightTheme] = useState(
-    localStorage.getItem("theme") === "light"
-  );
-
-  useEffect(() => {
-    if (isLightTheme) {
-      localStorage.setItem("theme", "light");
-    } else {
-      localStorage.setItem("theme", "dark");
-    }
-  }, [isLightTheme]);
-
-  function toggleTheme() {
-    const root = document.documentElement;
-    const savedTheme = localStorage.getItem("theme");
-    if (savedTheme === "light") {
-      root.classList.add("light");
-    } else {
-      root.classList.remove("light");
-    }
-    setIsLightTheme((prevIsLightTheme) => !prevIsLightTheme);
-  }
   return (
     <div className="w-full max-w-xl mx-auto">
-      <Header toggleTheme={toggleTheme} isLightTheme={isLightTheme} />
+      <Header />
       <Main />
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,11 @@
-"use client";
 import { FaMoon } from "react-icons/fa";
 import { IoSunny } from "react-icons/io5";
 
-interface HeaderProps {
-  toggleTheme: () => void;
-  isLightTheme: boolean;
-}
-
-export function Header({
-  toggleTheme,
-  isLightTheme,
-}: HeaderProps): JSX.Element {
+export function Header() {
   return (
     <header className="text-white text-2xl flex items-center justify-between py-11">
       <h1 className="font-bold uppercase tracking-[0.5rem]">Todo</h1>
-      <button onClick={toggleTheme}>
-        {isLightTheme ? <FaMoon /> : <IoSunny className="text-3xl" />}
-      </button>
+      <button>tes</button>
     </header>
   );
 }
