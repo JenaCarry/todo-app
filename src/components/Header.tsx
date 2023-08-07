eed922d70f7fc81940078e185d9add789947f53d
Jean <120676537+JenaCarry@users.noreply.github.com>
2023-08-07 20:18:58 -0300

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,31 @@
+"use client";
+import { useEffect, useState } from "react";
 import { FaMoon } from "react-icons/fa";
 import { IoSunny } from "react-icons/io5";
 
 export function Header() {
+  const [isLightTheme, setIsLightTheme] = useState(false);
+
+  useEffect(() => {
+    const root = document.documentElement;
+
+    if (isLightTheme) {
+      root.classList.add("light");
+    } else {
+      root.classList.remove("light");
+    }
+  }, [isLightTheme]);
+
+  function toggleTheme() {
+    setIsLightTheme((prevIsLightTheme) => !prevIsLightTheme);
+  }
+
   return (
     <header className="text-white text-2xl flex items-center justify-between py-11">
       <h1 className="font-bold uppercase tracking-[0.5rem]">Todo</h1>
-      <button>tes</button>
+      <button onClick={toggleTheme}>
+        {isLightTheme ? <FaMoon /> : <IoSunny className="text-3xl" />}
+      </button>
     </header>
   );
 }
