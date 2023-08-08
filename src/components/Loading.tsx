3b44802b186d013bf5e9150cdadbda903b7e1aaf
Jean <120676537+JenaCarry@users.noreply.github.com>
2023-08-08 01:48:37 -0300

    .

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,5 +1,7 @@
 export function Loading() {
   return (
-    <div className="animate-spin w-20 h-20 border-8 border-white/50 border-t-blue-600 rounded-full mx-auto"></div>
+    <div className="animate-spin w-20 h-20 border-8 border-white/50 border-t-blue-600 rounded-full mx-auto">
+      test
+    </div>
   );
 }
