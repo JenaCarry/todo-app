e3de4ad2127fa38cfda5da944ad57dd9d7576320
Jean <120676537+JenaCarry@users.noreply.github.com>
2023-08-08 01:34:13 -0300

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,32 @@
 "use client";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import { FaMoon } from "react-icons/fa";
 import { IoSunny } from "react-icons/io5";
 
 export function Header() {
-  const [theme, setTheme] = useState("");
+  const [isLightTheme, seIsLightTheme] = useState(true);
+
+  useEffect(() => {
+    const data = localStorage.getItem("theme");
+    if (data !== null) seIsLightTheme(JSON.parse(data));
+  }, []);
 
   useEffect(() => {
     const element = document.documentElement;
-    if (theme === "light") {
-      element.classList.add("light");
-      localStorage.setItem("theme", "light");
-    } else {
+    localStorage.setItem("theme", JSON.stringify(isLightTheme));
+    if (isLightTheme) {
       element.classList.remove("light");
-      localStorage.removeItem("theme");
+    } else {
+      element.classList.add("light");
     }
-  }, [theme]);
+  }, [isLightTheme]);
 
   return (
     <header className="text-white text-2xl flex items-center justify-between py-11">
       <h1 className="font-bold uppercase tracking-[0.5rem]">Todo</h1>
-      {theme === "light" ? (
-        <button onClick={() => setTheme("dark")}>
-          <FaMoon />
-        </button>
-      ) : (
-        <button onClick={() => setTheme("light")}>
-          <IoSunny className="text-3xl" />
-        </button>
-      )}
+      <button onClick={() => seIsLightTheme((current) => !current)}>
+        {isLightTheme ? <FaMoon /> : <IoSunny />}
+      </button>
     </header>
   );
 }
