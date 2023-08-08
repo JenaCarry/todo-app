14b79cc17f6ffe968b53f16f379cdd76b7ff629f
Jean <120676537+JenaCarry@users.noreply.github.com>
2023-08-07 21:49:23 -0300

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,7 @@ import { FaMoon } from "react-icons/fa";
 import { IoSunny } from "react-icons/io5";
 
 export function Header() {
-  const savedTheme = localStorage.getItem("theme");
-  const [isLightTheme, setIsLightTheme] = useState(savedTheme === "light");
+  const [isLightTheme, setIsLightTheme] = useState(false);
 
   useEffect(() => {
     const root = document.documentElement;
