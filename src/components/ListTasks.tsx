5bd0495b6aa6e4cccf35a7a1842161ed914ee7f7
Jean <120676537+JenaCarry@users.noreply.github.com>
2023-08-12 21:45:29 -0300

    delay 250 for 100

--- src/components/ListTasks.tsx
+++ src/components/ListTasks.tsx
@@ -40,7 +40,7 @@ export function ListTasks({ todos, setTodosAndSave }: ListTasksProps) {
 
   const touchSensor = useSensor(TouchSensor, {
     activationConstraint: {
-      delay: 250,
+      delay: 100,
       tolerance: 5,
     },
   });
